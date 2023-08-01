5829bee442168808dec0f3cf4fe846506616a38a
Calum Laverick <calumlaverick09@gmail.com>
2023-08-01 18:59:33 +0100

    improved search functionality

--- resources/main.js
+++ resources/main.js
@@ -1,23 +1,25 @@
 let filmNameRef = document.getElementById("film-name");
 let searchButton = document.getElementById("search-button");
 let result = document.getElementById("result");
+let input = document.getElementById("film-name");
 let apiKey = 24440972;
 
 // function to fetch data from the film api
 
 let getFilm = () => {
-    let filmName = filmNameRef.value;
-    let url = `https://www.omdbapi.com/?t=${filmName}&apikey=${apiKey}`;
-    
-    // If the input field is empty, do this
-    if(filmName.length <= 0) {
-        result.innerHTML = '<h3 class="msg">Please enter a movie name </h3>';
-    } else {
-        fetch(url).then((resp) => resp.json()).then((data) => {
+  let filmName = filmNameRef.value;
+  let url = `https://www.omdbapi.com/?t=${filmName}&apikey=${apiKey}`;
 
-            // If the film exists
-            if(data.Response == "True") {
-                result.innerHTML = `<div class="info"> 
+  // If the input field is empty, do this
+  if (filmName.length <= 0) {
+    result.innerHTML = '<h3 class="msg">Please enter a movie name </h3>';
+  } else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        // If the film exists
+        if (data.Response == "True") {
+          result.innerHTML = `<div class="info"> 
                 <img src=${data.Poster} class="poster">
                 <div>
                     <h2>${data.Title}</h2>
@@ -43,23 +45,36 @@ let getFilm = () => {
                 <h3>Cast:</h3>
                 <p> ${data.Actors} </p>
                 `;
-            } else {
-                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
-            }
-        })
+        } else {
+          result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+        }
+      })
 
-              // if any errors arise
-              .catch(() => {
-                result.innerHTML = `<h3 class"msg"> Error Occured </h3>`;
-            });
+      // if any errors arise
+      .catch(() => {
+        result.innerHTML = `<h3 class"msg"> Error Occured </h3>`;
+      });
+  }
+};
 
-    }
-
+// Clear the input value after pressing the button
+const Clearinput = () => {
+  input.value = "";
 };
 
 // Adding an event listener to the search button
 
-searchButton.addEventListener("click", getFilm);
+input.addEventListener("keydown", function (event) {
+  // Check if the Enter key was pressed
+  if (event.key === "Enter") {
+    // Your action here
+    getFilm();
+    Clearinput();
+  }
+});
+
+searchButton.addEventListener("click", function () {
+  getFilm();
+  Clearinput();
+});
 window.addEventListener("load", getFilm);
-
-
